5204f6dc95176dba05ed4b4a0b160e8333382d4a
MIGOSOFT\Marvin_Wu <mshiun.wu@gmail.com>
2016-10-19 14:19:06 +0800

    no message

---
+++ makeBlogPostfromRMarkdown.R
@@ -0,0 +1,61 @@
+.libPaths(c("d:/0.R_packages", .libPaths()))
+
+myjekyllsite = c("D:/MarvinWuPersonal/MarvinBlog/myJekyllBlog/")
+
+KnitPost <- function(input, base.url = myjekyllsite) {
+  require(knitr)
+
+  opts_knit$set(base.url = base.url)
+  fig.path <- paste0("figures/", sub(".Rmd$", "", basename(input)), "/")
+  opts_chunk$set(fig.path = fig.path)
+  opts_chunk$set(fig.cap = "center")
+  render_jekyll()
+  knit(input, envir = parent.frame())
+}
+
+KnitPost(input = "D:/MarvinWuPersonal/MarvinBlog/myJekyllBlog/_posts/2016-10-03-rmd-test.Rmd")
+
+
+
+
+
+#----------------------------------------#
+library(servr)
+
+servr::jekyll(dir = "D:/MarvinWuPersonal/MarvinBlog/myJekyllBlog/",
+              input = '_source',
+              output = '_posts',
+              serve = FALSE)
+
+jekyll <- 
+function (dir = ".", 
+          input = c(".", "_source", "_posts"), 
+          output = c(".",  "_posts", "_posts"), 
+          script = c("Makefile", "build.R"), 
+          serve = TRUE, 
+          command = "jekyll build", ...) 
+{
+  baseurl = jekyll_config(dir, "baseurl", "")
+  destination = jekyll_config(dir, "destination", "_site")
+  jekyll_build = function() {
+    if (system(command) != 0) 
+      stop("Failed to run: ", command)
+  }
+  build_all = function() knit_maybe(input, output, script, 
+                                    method = "jekyll")
+  if (!serve) {
+    in_dir(dir, {
+      build_all()
+      jekyll_build()
+    })
+    return()
+  }
+  dynamic_site(dir, ..., build = function(...) {
+    update = build_all()
+    if (update || !file_test("-d", destination)) 
+      jekyll_build()
+    update
+  }, site.dir = destination, baseurl = baseurl)
+}
+
+
